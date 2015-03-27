84a8a7b7159a4a0db6e3892a62e9a94319bca746
arvindcj <code@arvindcj.com>
2015-03-28 10:48:13 +1100

    + travis CI

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,2 +1,10 @@
-task :test do
+
+begin
+  require 'rspec/core/rake_task'
+
+  RSpec::Core::RakeTask.new(:spec)
+
+  task :default => :spec
+rescue LoadError
+  # no rspec available
 end
---
+++ spec/usage_spec.rb
@@ -0,0 +1,5 @@
+describe "usage" do 
+  it "test" do
+    expect("test").to have_content "test"
+  end
+end
